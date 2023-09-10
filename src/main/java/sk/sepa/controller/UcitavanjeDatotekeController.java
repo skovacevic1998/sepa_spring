bf17e5a6bceedadd5541163aa94c64af3ede7327
Siniša Kovačević <73254143+skovacevic7@users.noreply.github.com>
2023-09-10 23:58:26 +0200

    Implementacija datotečnog unosa naloga.

--- src/main/java/sk/sepa/controller/UcitavanjeDatotekeController.java
+++ src/main/java/sk/sepa/controller/UcitavanjeDatotekeController.java
@@ -1,39 +1,67 @@
 package sk.sepa.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
+import java.io.File;
 import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile; // Import MultipartFile
+import sk.sepa.object.nalog.Nalog;
+import sk.sepa.schema.generated.Document;
 
 @RestController
 @RequestMapping("/api")
 public class UcitavanjeDatotekeController {
     @PostMapping("/sepaValidation")
-    public String validateXml(@RequestBody String xmlData) {
+    public ResponseEntity<Object> validateXml(@RequestPart("file") MultipartFile file) {
         try {
-            // Load the XSD schema
+            // Specify the file path to the XSD schema
+            String xsdFilePath = "D:/code/spring/sepa_spring/src/main/java/sk/sepa/schema/pain.001.001.03.xsd";
+
+            // Load the XSD schema from the file path
             SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = schemaFactory.newSchema(getClass().getResource("/sk/sepa/schema/pain.001.001.03.xsd"));
+            Schema schema = schemaFactory.newSchema(new File(xsdFilePath));
 
             // Create a validator
             Validator validator = schema.newValidator();
 
-            // Perform validation
-            validator.validate(new StreamSource(new java.io.StringReader(xmlData)));
+            // Perform validation on the uploaded XML file
+            validator.validate(new StreamSource(file.getInputStream()));
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            // Parse the uploaded XML file into a Document
+            Document document = (Document) builder.parse(new InputSource(file.getInputStream()));
+
+
+
+
+
 
             // Validation successful
-            return "XML is valid according to the schema.";
+            return ResponseEntity.ok("XML is valid according to the schema."+ document);
         } catch (SAXException | IOException e) {
             // Validation failed
-            return "XML validation failed: " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("XML validation failed: " + e.getMessage());
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
         }
     }
 }
